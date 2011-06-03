b0659b2e7be47fb72c7e2b0607ece29e9041c685
Zak van der Merwe <zakvdm@gmail.com>
2011-06-03 13:49:30 +0200

    Added Agnes month.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import cgi
-import datetime
+from datetime import date
 import urllib
 import logging
 import wsgiref.handlers
@@ -14,15 +14,27 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-namesDictionary = {"chris":"Chris", "nick":"Nick", "steve":"Steve", "zak":"Zak"}
+CHRIS, NICK, STEVE, ZAK, NOBODY = "chris", "nick", "steve", "zak", "nobody"
+
+namesDictionary = {CHRIS:"Chris", NICK:"Nick", STEVE:"Steve", ZAK:"Zak"}
 dayDictionary = {"monday":"Monday", "tuesday":"Tuesday", "wednesday":"Wednesday", "thursday":"Thursday", "friday":"Friday", "saturday":"Saturday", "sunday":"Sunday", "noday":"Not Cooking"}
 notificationRecipients = "'Chris <chrisdk@gmail.com>', 'Nicholas <nicholas.savage@gmail.com>', 'Stephen <sasherson@gmail.com>', 'Zak <zakvdm@gmail.com>'"
 
+AGNES_PERK = "AGNES"
+
+# DATASTORE ENTITIES:
 class Schedule(db.Model):
     json = db.StringProperty()
 
+class Perk(db.Model):
+    start = db.DateProperty(required=True)
+    type = db.StringProperty(required=True, choices=set([AGNES_PERK]))
+    owner = db.StringProperty(required=True, choices=set([CHRIS, NICK, STEVE, ZAK, NOBODY]))
+
+# REQUEST HANDLERS:
 class MainPage(webapp.RequestHandler):
     def get(self):
+   
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
             url_text = "logout"
@@ -32,14 +44,50 @@ class MainPage(webapp.RequestHandler):
             url_text = "login"
             user_status = "out"
 
+        agnes_turn, agnes_progress, next_agnes_turn = self.agnesTurn()
+
         template_values = {
                  'url': url,
                  'url_text': url_text,
                  'user_status': user_status,
+                 'agnes_turn': agnes_turn,
+                 'agnes_progress': agnes_progress,
+                 'next_agnes_turn': next_agnes_turn,
              }
 
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
+
+    def agnesTurn(self):
+        perk_count = Perk.all().count(1)
+        if perk_count == 0:
+            initialPerk = Perk(start=date(2001, 1, 1), type=AGNES_PERK, owner=NOBODY)
+            initialPerk.put()
+
+        today = date.today()
+        perks = Perk.all().filter('start <=', today).order('-start').fetch(1)
+
+        agnes_turn = "nobody"
+        for perk in perks:
+            current_perk = perk
+            agnes_turn = namesDictionary[perk.owner]
+            break
+
+        coming_perks = Perk.all().filter('start >', today).order('start').fetch(1)
+
+        progress = 0
+        next_agnes_turn = "nobody"
+        for coming_perk in coming_perks:
+            next_agnes_turn = namesDictionary[coming_perk.owner]
+            # Percent complete:
+            total_duration = (coming_perk.start - current_perk.start).total_seconds()
+            time_so_far = (today - current_perk.start).total_seconds()
+            progress = int((time_so_far / total_duration) * 100)
+            logging.info("Calculated Agnes progress: [total_duration = " + str(total_duration) + "][ time_so_far = " + str(time_so_far) + "][ progress = " + str(progress) + "]")
+            break
+
+        return agnes_turn, progress, next_agnes_turn
+        
 
 class LoadSchedule(webapp.RequestHandler):
     def get(self):
@@ -88,10 +136,10 @@ class SaveSchedule(webapp.RequestHandler):
         self.response.out.write("{}")
 
     def notify(self, schedule):
-        chrisSchedule = namesDictionary["chris"] + " - " + dayDictionary[schedule["chris"]]
-        nickSchedule = namesDictionary["nick"] + " - " + dayDictionary[schedule["nick"]]
-        steveSchedule = namesDictionary["steve"] + " - " + dayDictionary[schedule["steve"]]
-        zakSchedule = namesDictionary["zak"] + " - " + dayDictionary[schedule["zak"]]
+        chrisSchedule = namesDictionary[CHRIS] + " - " + dayDictionary[schedule[CHRIS]]
+        nickSchedule = namesDictionary[NICK] + " - " + dayDictionary[schedule[NICK]]
+        steveSchedule = namesDictionary[STEVE] + " - " + dayDictionary[schedule[STEVE]]
+        zakSchedule = namesDictionary[ZAK] + " - " + dayDictionary[schedule[ZAK]]
         
         body = "The new shedule is:" + "\n  " + chrisSchedule + "\n  " + nickSchedule + "\n  " + steveSchedule + "\n  " + zakSchedule + "\n\n ~ http://spagbolroster.appspot.com"
 
